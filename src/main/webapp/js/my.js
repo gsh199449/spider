e628e630b999d568a1ead3b4340dbb063c41fffe
zhaotiantian <ttzhao@v5.cn>
2017-02-07 10:31:10 +0800

    改一下

--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -1,6 +1,7 @@
 /**
  * Created by gaoshen on 16/5/17.
  */
+ var needShowResultModel = false;
 function showModal(title, content) {
     $("#modalTitle").text(title);
     $("#modalBody").html(content);
@@ -33,12 +34,18 @@ function rpc(url, pram, callback) {
     $("#confirmModalBody").html("确定要执行" + url + "吗?");
     $("#confirmButton").one("click", function () {
         $('#confirmModal').modal('hide');
-        $.getJSON(url, pram, callback);
+        needShowResultModel = true;
     });
+    $("#confirmButton").on('hidden.bs.modal', function  () {
+        if (needShowResultModel) {
+            $.getJSON(url, pram, callback);
+        };
+    })
     $('#confirmModal').modal('show');
 }
 function rpcAndShowData(url, pram) {
     rpc(url, pram, function (data) {
+        needShowResultModel = false;
         if (data.success) {
             showModal("成功", data.result != undefined ? data.result : data.resultList);
         } else {
