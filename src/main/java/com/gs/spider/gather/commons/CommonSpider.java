6a4c485348375808d7101ee88a17f2cf609a183e
striker <wlluo@v5.cn>
2017-02-09 10:37:13 +0800

    正则和xpath未抽取到category，就用默认的category

--- src/main/java/com/gs/spider/gather/commons/CommonSpider.java
+++ src/main/java/com/gs/spider/gather/commons/CommonSpider.java
@@ -269,6 +269,8 @@ public class CommonSpider extends AsyncGather {
             }
             if (StringUtils.isNotBlank(category)) {
                 page.putField("category", category);
+            }else {
+                page.putField("category", info.getDefaultCategory());
             }
 
             //抽取发布时间
